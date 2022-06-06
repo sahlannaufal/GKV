b88331df4de72b1efa369591956b6cd762e473a0
I Gusti Agung Ngurah Fajar Dharmawangsa <96526566+NgurahFajar@users.noreply.github.com>
2022-06-06 10:57:56 +0700

    Update line.js

--- line.js
+++ line.js
@@ -21,10 +21,12 @@ d3.csv('https://raw.githubusercontent.com/NgurahFajar/GKV/main/covid_19_indonesi
     title: 'Daily New Recovered',
   };
 
-  Plotly.newPlot('Line1', data, layout);
+  var config = { responsive: true };
+
+  Plotly.newPlot('Line1', data, layout, config);
 });
 
-// Daily New Recovered
+// Daily New Cases
 d3.csv('https://raw.githubusercontent.com/NgurahFajar/GKV/main/covid_19_indonesia_time_series_all.csv', function (err, rows) {
   function unpack(rows, key) {
     return rows.map(function (row) {
@@ -35,17 +37,45 @@ d3.csv('https://raw.githubusercontent.com/NgurahFajar/GKV/main/covid_19_indonesi
   var trace2 = {
     type: 'scatter',
     mode: 'lines',
-    name: 'Daily New Recovered',
+    name: 'Daily New Cases',
     x: unpack(rows, 'Date'),
-    y: unpack(rows, 'New Recovered'),
-    line: { color: 'green' },
+    y: unpack(rows, 'New Cases'),
+    line: { color: 'orange' },
   };
 
   var data2 = [trace2];
 
   var layout2 = {
-    title: 'Daily New Recovered',
+    title: 'Daily New Cases',
+  };
+  var config2 = { responsive: true };
+
+  Plotly.newPlot('Line2', data2, layout2, config2);
+});
+
+// Daily New Deaths
+d3.csv('https://raw.githubusercontent.com/NgurahFajar/GKV/main/covid_19_indonesia_time_series_all.csv', function (err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function (row) {
+      return row[key];
+    });
+  }
+
+  var trace3 = {
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Daily New Deaths',
+    x: unpack(rows, 'Date'),
+    y: unpack(rows, 'New Deaths'),
+    line: { color: 'red' },
   };
 
-  Plotly.newPlot('Line2', data2, layout2);
+  var data3 = [trace3];
+
+  var layout3 = {
+    title: 'Daily New Deaths',
+  };
+  var config3 = { responsive: true };
+
+  Plotly.newPlot('Line3', data3, layout3, config3);
 });
